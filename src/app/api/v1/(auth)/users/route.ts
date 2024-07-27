3cd28433e0dcc5bf57024dc7ecc53003a8792aee
roushan-sheik <roushansheik@gmail.com>
2024-07-27 18:04:04 +0600

    add: get all users

--- src/app/api/v1/(auth)/users/route.ts
+++ src/app/api/v1/(auth)/users/route.ts
@@ -1,5 +1,15 @@
+import connect from "@/lib/database/db";
+import User from "@/lib/models/User";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-  return new NextResponse("User Route");
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: any) {
+    return new NextResponse("Error in fetching users " +error.message, {
+      status: 500,
+    })
+  }
 };
---
+++ src/lib/models/User.ts
@@ -0,0 +1,25 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.User || model("User", UserSchema);
+export default User;

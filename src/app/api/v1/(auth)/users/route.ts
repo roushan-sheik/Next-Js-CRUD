969592c4c0f6a14df6a1ba7868f2b3cce658b885
roushan-sheik <roushansheik@gmail.com>
2024-07-27 18:24:09 +0600

    add: create user route

--- src/app/api/v1/(auth)/users/route.ts
+++ src/app/api/v1/(auth)/users/route.ts
@@ -1,15 +1,36 @@
 import connect from "@/lib/database/db";
 import User from "@/lib/models/User";
+
 import { NextResponse } from "next/server";
 
+// get all users route
 export const GET = async () => {
   try {
     await connect();
     const users = await User.find();
     return new NextResponse(JSON.stringify(users), { status: 200 });
   } catch (error: any) {
-    return new NextResponse("Error in fetching users " +error.message, {
+    return new NextResponse("Error in fetching users " + error.message, {
       status: 500,
-    })
+    });
+  }
+};
+// create user route
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    const newUser = new User(body);
+    await newUser.save();
+    return new NextResponse(
+      JSON.stringify({ message: "New user is created", user: newUser }),
+      { status: 201 }
+    );
+  } catch (error: any) {
+    return new NextResponse(
+      JSON.stringify({
+        error: "Error while creating user",
+        message: error.message,
+      })
+    );
   }
 };

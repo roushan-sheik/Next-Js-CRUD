1628b35457d9d5d50b2381e6871cc4514cd590f4
roushan-sheik <roushansheik@gmail.com>
2024-07-28 12:37:06 +0600

    add: blog schema model

---
+++ src/lib/models/Blog.ts
@@ -0,0 +1,25 @@
+import { Schema, model, models } from "mongoose";
+
+const BlogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { timestamps: true }
+);
+const Blog = models.Blog || model("Blog", BlogSchema);
+export default Blog;
